e5144297a36d4f3c243adbc2f5f2b998bb01eb2c
Eric Brody <eric@kitcheck.com>
2015-02-05 08:04:38 -0500

    Updating warnings and errors to properly reflect new functionality

--- tasks/slack.js
+++ tasks/slack.js
@@ -6,10 +6,17 @@ module.exports = function(grunt) {
     grunt.registerMultiTask('slack', 'Push info to slack', function() {
         var options = this.options();
 
-        // We are good to go
+        if(options.token || options.domain){
+            grunt.log.warn('Sending Slack web hooks via `token` and `domain` has been deprecated!');
+        }
+
+        if(!options.endpoint){
+            grunt.log.error('Slack web hook endpoint must be defined.');
+            return;
+        }
+
         var done = this.async(),
             message = grunt.option('message') || '',
-            url = options.endpoint ? options.endpoint : 'https://' + options.domain + '.slack.com/services/hooks/incoming-webhook?token=' + options.token,
             data = {
                 text: this.data.text.replace('{{message}}', message)
             };
@@ -28,7 +35,7 @@ module.exports = function(grunt) {
             data.icon_url = options.icon_url;
         }
 
-        request.post(url).type('form').send('payload=' + JSON.stringify(data)).end(function(res) {
+        request.post(options.endpoint).type('form').send('payload=' + JSON.stringify(data)).end(function(res) {
             if (!res.ok) {
                 grunt.log.error('Error sending message to slack: ', res.text);
                 return done(false);
